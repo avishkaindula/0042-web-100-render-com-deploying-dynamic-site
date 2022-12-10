12c6e43c504c575ffc6f34f6124e3d66154f61dd
Avishka Indula <avishkainduladev@gmail.com>
2022-12-10 08:53:57 +0530

    Testing, Code Preparation, Environment Variables
    => there are 5 steps we need to take before deploying a website
    1. Testing - Manual and Automated Testing
    2. Prepare code for deployment (e.g. use environment variables)
    -  i.  Remove and replace dummy values
    -  ii. Replace data/keys with real data/keys
    -  iii.Use environment variables for data that differs between development and deployment.
    -  iv. Consider using cloud storage providers for file uploads.
    3. Be aware of browser limitations and add cross-browser support
    -  i.  We can use caniuse.com to check browser support for various HTML, CSS and JS features.
    -  ii. We can also use MDN Docs for evaluating browser support for certain features.
    4. Search Engine Optimization (SEO)
    5. Improve performance and shrink frontend assets. (JS, Images, CSS)
    => First we can do manual testing and check for possible bugs.
    => in preparing step, we need to replace dummy values in our code with real values
    => we need to replace testing data/keys with real data/keys
    => for example, we need to replace the mongodb://localhost:27017 with the
    *  url provided by the cloud database hosting provider (MongoDB Atlas)
    => but manually updating this values can be error prone.
    => so in order to replace those values easily, we can use a technology called
    *  "environment variables". The idea here is that you can use a variable which is not set
    *  somewhere in your code, but instead is set in the environment. (the remote machine)
    *  and then it's inject into the running website code once it start running on that remote machine.
    => in order to implement that, first we need to replace mongodb://localhost:27017 of
    *  MongoClient.connect('mongodb://localhost:27017'); in database.js by creating a normal variable
    *  called let mongodbUrl = "mongodb://localhost:27017"; and then pass that to .connect like following.
    *  MongoClient.connect(mongodbUrl);
    => then we can create a if check in database.js and create MONGODB_URL environment variable inside that.
    => Now we also need to consider using a cloud storage provider for hosting images and other files that are
    *  uploaded by users. We can use Cloudinary for that.
    => they give us tools to forwarding the files that we got in our servers into their servers.
    => then they will give us urls which we can use in our templates. So then those files are directly
    *  requested from Cloudinary instead from our own local servers.

--- data/database.js
+++ data/database.js
@@ -1,17 +1,30 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
+
+let mongodbUrl = "mongodb://localhost:27017";
+
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+}
+// This is how we create a environment variable.
+// When we deploy this code to a remote machine, this will search for a environment
+// variable called MONGODB_URL in that machine. If such environment variable is found,
+// then the value of mongodbUrl will be replaced by the value of MONGODB_URL
+// As this environment variable doesn't exist on our local machine, "mongodb://localhost:27017"
+// will be used as the Url instead.
 
 let database;
 
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('deployment');
+  // const client = await MongoClient.connect("mongodb://localhost:27017");
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db("deployment");
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('No database connected!');
+    throw new Error("No database connected!");
   }
 
   return database;
